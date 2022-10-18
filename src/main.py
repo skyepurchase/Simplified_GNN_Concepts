0a207bc51d02a48b30add0f95bad035cb05be396
Skye Purchase <atp45@cam.ac.uk>
2022-10-18 17:21:58 +0100

    Test run with pytorch lightning wrapper and gcn

--- src/main.py
+++ src/main.py
@@ -1,24 +1,60 @@
-import torch
+from datetime import datetime
 
-from torch_geometric.utils import to_networkx
+from torch_geometric.datasets import Planetoid
 from torch_geometric.data import Data
 
-import networkx as nx
-import matplotlib.pyplot as plt
+from loaders import loaders
+from models import models
+from wrappers import wrappers
+
+from pytorch_lightning import loggers
+import pytorch_lightning as pl
+
+import os.path as osp
 
 
-def main():
-    edge_index = torch.tensor([[0, 1, 1, 2],
-                              [1, 0, 2, 1]], dtype=torch.long)
-    x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
+def main(root: str, name: str, experiment: str) -> None:
+    dataset = Planetoid(root=osp.join(root, name), name=name)
+    data = dataset[0]
 
-    data = Data(x=x, edge_index=edge_index)
+    loader = loaders["RandomNodeSampler"]
 
-    graph = to_networkx(data)
-    nx.draw(graph)
-    plt.savefig('test.png')
+    train_loader = loader(data,
+                          shuffle=True,
+                          num_parts=10,
+                          num_workers=16)
+    val_loader = loader(data,
+                        shuffle=False,
+                        num_parts=10,
+                        num_workers=16)
+
+    model = models["gcn"](dataset.num_node_features,
+                          dataset.num_classes)
+
+    pl_model = wrappers["Wrapper"](model, 0.01)
+
+    time = datetime.now()
+    version = f'{time.strftime("%Y%m%d-%H%M%S")}_1337'
+    tb_logger = loggers.TensorBoardLogger(save_dir="logs/",
+                                          name=experiment,
+                                          version=version)
+
+    trainer = pl.Trainer(
+        accelerator='cpu',
+        devices=1,
+        logger=tb_logger,
+        replace_sampler_ddp=False,
+        strategy='ddp',
+        max_epochs=10,
+        enable_progress_bar=True)
+
+    trainer.fit(pl_model, train_loader, val_loader)
+
+#     graph = to_networkx(data)
+#     nx.draw(graph)
+#     plt.savefig('test.png')
 
 
 if __name__=="__main__":
-    main()
+    main("data", "Cora", "setuptest")
 
