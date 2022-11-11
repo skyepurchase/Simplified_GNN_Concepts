7eca4310cac46a698deec0eb456fe326e5d71d2e
Skye Purchase <atp45@cam.ac.uk>
2022-11-11 00:59:43 +0000

    Switching to config file based training

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,6 @@
+from torch_geometric.data import InMemoryDataset
 from .syngraphs import SynGraph 
+from torch_geometric.datasets import Planetoid
 
 datasets = {
     "BA-Shapes": SynGraph,
@@ -6,5 +8,14 @@ datasets = {
     "BA-Community": SynGraph,
     "Tree-Shapes": SynGraph,
     "Tree-Grid": SynGraph,
+    "Cora": Planetoid,
 }
+
+def get_dataset(name: str, root: str) -> InMemoryDataset:
+    if name == "Cora":
+        return Planetoid(root, name)
+    elif name == "BA-Shapes":
+        return SynGraph(root)
+    else:
+        raise ValueError(f'Unsupported dataset {name}')
 
--- src/main.py
+++ src/main.py
@@ -1,60 +1,101 @@
 from datetime import datetime
+from torch import Tensor
 
 from torch_geometric.datasets import Planetoid
-from torch_geometric.data import Data
+from torch_geometric.data import Data, InMemoryDataset
+from torch_sparse import SparseTensor
 
 from loaders import loaders
-from models import models
+from datasets import get_dataset 
+from models import models, sgc
 from wrappers import wrappers
 
-from pytorch_lightning import loggers
+from pytorch_lightning import loggers, seed_everything
 import pytorch_lightning as pl
 
 import os.path as osp
+from typing import Union
 
 
-def main(root: str, name: str, experiment: str) -> None:
-    dataset = Planetoid(root=osp.join(root, name), name=name)
-    data = dataset[0]
+DIR = osp.dirname(__file__)
 
-    loader = loaders["RandomNodeSampler"]
+
+def main(experiment: str,
+         config,
+         args) -> None:
+    seed_everything(args.seed)
+
+    dataset: InMemoryDataset = get_dataset(args.dataset,
+                                           osp.join(DIR,
+                                                    args.root,
+                                                    args.dataset))
+    temp = dataset[0]
+    if isinstance(temp, Data):
+        data: Union[Data, Tensor] = temp
+    else:
+        raise ValueError('Expected dataset at index zero to be type {Data} received type {type(temp)}')
+
+    model = models[config["model"]["name"]](dataset.num_features,
+                                            dataset.num_classes,
+                                            **config["model"]["kwargs"])
+
+    pl_model = wrappers[config["wrapper"]["name"]](model, config["wrapper"]["learning_rate"])
+
+    loader = loaders[config["sampler"]["name"]]
 
     train_loader = loader(data,
                           shuffle=True,
-                          num_parts=10,
+                          **config["sampler"]["train"],
                           num_workers=16)
     val_loader = loader(data,
                         shuffle=False,
-                        num_parts=10,
+                        **config["sampler"]["val"],
                         num_workers=16)
 
-    model = models["gcn"](dataset.num_node_features,
-                          dataset.num_classes)
-
-    pl_model = wrappers["Wrapper"](model, 0.01)
-
     time = datetime.now()
-    version = f'{time.strftime("%Y%m%d-%H%M%S")}_1337'
-    tb_logger = loggers.TensorBoardLogger(save_dir="logs/",
+    version = f'{time.strftime("%Y%m%d-%H%M%S")}_{args.seed}'
+    tb_logger = loggers.TensorBoardLogger(save_dir=osp.join(DIR,
+                                                            config["trainer"]["dir"]),
                                           name=experiment,
                                           version=version)
 
     trainer = pl.Trainer(
-        accelerator='cpu',
-        devices=1,
+        accelerator=config["trainer"]["accelerator"],
+        devices=config["trainer"]["devices"],
         logger=tb_logger,
         replace_sampler_ddp=False,
         strategy='ddp',
-        max_epochs=10,
+        max_epochs=config["trainer"]["max_epochs"],
         enable_progress_bar=True)
 
     trainer.fit(pl_model, train_loader, val_loader)
 
 #     graph = to_networkx(data)
 #     nx.draw(graph)
+#     if config["model"]["name"] == "SGC":
+#         adjacency: Tensor  = SparseTensor(row=data.edge_index[0], col=data.edge_index[1]).to_dense()
+#         norm_adj: Tensor = sgc.normalize_adjacency(adjacency)
+#         data, precompute_time = sgc.precompute_features(data.x, norm_adj, config["model"]["degree"])
+#         print(f'PRECOMPUTE TIME: {precompute_time}')
+
 #     plt.savefig('test.png')
 
 
 if __name__=="__main__":
-    main("data", "Cora", "setuptest")
+    
+    import yaml
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True, help="Config file")
+    parser.add_argument('--dataset', required=True, help="Dataset to run on")
+    parser.add_argument('--root', required=True, help="Root directory for dataset")
+    parser.add_argument('--seed', required=True, type=int, help="Seed for randomisation")
+    args = parser.parse_args()
+
+    with open(osp.abspath(args.config), 'r') as config_file:
+        config = yaml.safe_load(config_file)
+        filename = args.config.split('/')[-1]
+        expr_name = args.dataset + "." + '.'.join(filename.split('.')[:-1])
+        main(expr_name, config, args)
 
