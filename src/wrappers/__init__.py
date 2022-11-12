04ace73214715acd16173f127b8bf924e33c3be4
Skye Purchase <atp45@cam.ac.uk>
2022-11-12 14:40:48 +0000

    Adding non-graph pytorch lightning wrapper

--- src/wrappers/__init__.py
+++ src/wrappers/__init__.py
@@ -1,12 +1,14 @@
-from .pl_wrapper import PLModel
+from .pl_wrapper import LinearWrapper, GraphWrapper 
 import torch.nn as nn
 
 
 def get_wrapper(name: str,
                 model: nn.Module,
                 learning_rate: int):
-    if name == "Wrapper":
-        return PLModel(model, learning_rate)
+    if name == "Graph":
+        return GraphWrapper(model, learning_rate)
+    if name == "Linear":
+        return LinearWrapper(model, learning_rate)
     else:
         raise ValueError(f'Unsupported model wrapper {name}')
 
