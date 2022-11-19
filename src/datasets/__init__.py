6587289a839ccbbb15e0488d144017daedfe57e3
Skye Purchase <atp45@cam.ac.uk>
2022-11-19 16:27:43 +0000

    Added Reddit dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,6 @@
 from torch_geometric.data import InMemoryDataset
 from .syngraphs import SynGraph 
-from torch_geometric.datasets import Planetoid
+from torch_geometric.datasets import Planetoid, Reddit
 
 datasets = {
     "BA-Shapes": SynGraph,
@@ -8,12 +8,13 @@ datasets = {
     "BA-Community": SynGraph,
     "Tree-Shapes": SynGraph,
     "Tree-Grid": SynGraph,
-    "Cora": Planetoid,
 }
 
 def get_dataset(name: str, root: str) -> InMemoryDataset:
-    if name == "Cora":
+    if name in ["Cora", "PubMed", "CiteSeer"]:
         return Planetoid(root, name)
+    elif name == "Reddit":
+        return Reddit(root)
     elif name == "BA-Shapes":
         return SynGraph(root)
     else:
