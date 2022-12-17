52479eda78bb255b8a13d716cb588f447c77e3c2
Skye Purchase <atp45@cam.ac.uk>
2022-12-17 20:53:52 +0000

    Adding more flexibility in GCN model generation

--- src/models/gcn.py
+++ src/models/gcn.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F 
 from torch_geometric.nn import GCNConv
+from torch.nn import Linear
 
 # typing
 from torch import Tensor
@@ -11,40 +12,51 @@ class GCN(nn.Module):
     def __init__(self, 
                  in_channels : int,
                  out_channels : int,
-                 num_layers : int = 1,
-                 hid_features : int = 16,
-                 dropout : float = 0.2):
+                 num_conv_layers : int = 1,
+                 num_lin_layers : int = 1,
+                 pooling : bool = False,
+                 hid_features : list = [16]):
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.num_layers = num_layers
-        self.dropout = dropout
+        self.num_conv_layers = num_conv_layers
+        self.num_lin_layers = num_lin_layers
+        self.pooling = pooling
 
         self.layers = nn.ModuleList() 
-        in_features = in_channels
-        for _ in range(num_layers):
+        in_features: int = in_channels
+        for i in range(num_conv_layers):
             conv = GCNConv(in_features,
-                           hid_features)
-            self.layers.append(conv)
-            in_features = hid_features
+                           hid_features[i])
+            conv.reset_parameters()
 
-        output_layer = GCNConv(in_features,
-                               out_channels)
+            self.layers.append(conv)
+            in_features = hid_features[i]
+
+        for i in range(num_lin_layers - 1):
+            lin = Linear(in_features,
+                         hid_features[num_conv_layers + i])
+            lin.reset_parameters()
+
+            self.layers.append(lin)
+            in_features = hid_features[num_conv_layers + i]
+
+        output_layer = Linear(in_features,
+                              out_channels)
         self.layers.append(output_layer)
 
-        print(self.layers)
-
     def forward(self, x : Tensor, edge_index: Adj) -> Tensor:
-        for i, conv in enumerate(self.layers):
-            x = conv(x, edge_index)
-            if i != len(self.layers) - 1:
+        for i, layer in enumerate(self.layers):
+            x = layer(x, edge_index)
+            if i < self.num_conv_layers:
                 x = F.relu(x)
-                x = F.dropout(x, p=self.dropout, training=self.training)
 
         return F.log_softmax(x, dim=-1)
 
     def __repr__(self) -> str:
-        return (f'{self.__class__.__name__}({self.in_channels}), '
-                f'{self.out_channels}, num_layers={self.num_layers}')
+        pooling = ' global max. pooling,' if self.pooling else ''
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, '
+                f'num_layers=[{self.num_conv_layers},{pooling} {self.num_lin_layers})')
 
