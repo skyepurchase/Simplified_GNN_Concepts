5830a8b077d2b4ff9fb07d292e4b500a8e4de82d
Skye Purchase <atp45@cam.ac.uk>
2022-12-29 18:33:23 +0000

    New wrapper for inference training (Reddit-binary and mutagenicity)

--- src/wrappers/pl_wrapper.py
+++ src/wrappers/pl_wrapper.py
@@ -75,48 +75,8 @@ class GraphWrapper(pl.LightningModule):
                                               correct * 100/total,
                                               self.current_epoch)
 
-#    def validation_step(self, batch, batch_idx: int) -> BatchDict:
-#        z: Tensor = self.model(batch.x, batch.edge_index)[batch.val_mask]
-#        y: Tensor = batch.y[batch.val_mask]
-#
-#        correct: Number = z.argmax(dim=1).eq(y).sum().item()
-#        total: int = len(y)
-
-#        val_loss: Tensor = self.criterion(z, y)
-#        val_acc: Tensor = z.argmax(dim=1).eq(y).sum()/len(y)
-
-#        self.log("val_acc", val_acc*100, prog_bar=True)
-
-#        logs = {"val_loss": val_loss,
-#                "val_acc": val_acc}
-
-#        batch_dictionary : BatchDict = {
-#            'loss': val_loss,
-#            'acc': val_acc,
-#            'log': logs,
-#            'correct': correct,
-#            'total': total
-#        }
-#
-#        return batch_dictionary
-
-#    def validation_epoch_end(self, outputs: list[BatchDict]) -> None:
-#        avg_loss: Tensor = torch.stack([x['loss'] for x in outputs]).mean()
-
-#        correct: Number = sum([x['correct'] for x in outputs])
-#        total: int = sum([x['total'] for x in outputs])
-
-#        if isinstance(self.logger, TensorBoardLogger):
-#            self.logger.experiment.add_scalar("Loss/Validation",
-#                                              avg_loss,
-#                                              self.current_epoch)
-
-#            self.logger.experiment.add_scalar("Accuracy/Validation",
-#                                              correct * 100/total,
-#                                              self.current_epoch)
-
     def test_step(self, batch, batch_idx: int) -> BatchDict:
-        z: Tensor = self.model(batch.x, batch.edge_index)[batch.test_mask]
+        z: Tensor = self.model(batch.x, batch.edge_index, batch.batch)[batch.test_mask]
         y: Tensor = batch.y[batch.test_mask]
 
         correct: Number = z.argmax(dim=1).eq(y).sum().item()
@@ -124,6 +84,103 @@ class GraphWrapper(pl.LightningModule):
 
         test_loss: Tensor = self.criterion(z, y)
         test_acc: Tensor = z.argmax(dim=1).eq(y).sum()/len(y)
+
+        self.log("test_acc", test_acc*100, prog_bar=True)
+
+        logs = {"test_loss": test_loss,
+                "test_acc": test_acc}
+
+        batch_dictionary : BatchDict = {
+            'loss': test_loss,
+            'acc': test_acc,
+            'log': logs,
+            'correct': correct,
+            'total': total
+        }
+
+        return batch_dictionary
+
+    def test_epoch_end(self, outputs: list[BatchDict]) -> None:
+        avg_loss: Tensor = torch.stack([x['loss'] for x in outputs]).mean()
+
+        correct: Number = sum([x['correct'] for x in outputs])
+        total: int = sum([x['total'] for x in outputs])
+
+        if isinstance(self.logger, TensorBoardLogger):
+            self.logger.experiment.add_scalar("Loss/Test",
+                                              avg_loss,
+                                              self.current_epoch)
+
+            self.logger.experiment.add_scalar("Accuracy/Test",
+                                              correct * 100/total,
+                                              self.current_epoch)
+
+
+class GraphPoolWrapper(pl.LightningModule):
+    def __init__(self,
+                 model: Module,
+                 learning_rate: float) -> None:
+        super().__init__()
+        self.lr: float = learning_rate
+        self.model: Module = model
+        self.criterion: Callable[[Tensor, Tensor], Tensor] = torch.nn.BCEWithLogitsLoss()
+
+    def forward(self, batch) -> torch.Tensor:
+        return self.model(batch.x, batch.edge_index)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return {'optimizer': optimizer}
+
+    def training_step(self, batch, batch_idx: int) -> BatchDict:
+        z: Tensor = self.model(batch.x, batch.edge_index, batch.batch)
+        one_hot = torch.nn.functional.one_hot(batch.y, num_classes=2).type_as(z)
+
+        correct: Number = z.argmax(dim=1).eq(batch.y).sum().item()
+        total: int = len(batch.y)
+
+        train_loss: Tensor = self.criterion(z, one_hot)
+        train_acc: Tensor = z.argmax(dim=1).eq(batch.y).sum()/len(batch.y)
+
+        self.log("train_acc", train_acc*100, prog_bar=True)
+
+        logs = {"train_loss": train_loss,
+                "train_acc": train_acc}
+
+        batch_dictionary : BatchDict = {
+            'loss': train_loss,
+            'acc': train_acc,
+            'log': logs,
+            'correct': correct,
+            'total': total
+        }
+
+        return batch_dictionary
+
+    def train_epoch_end(self, outputs: list[BatchDict]) -> None:
+        avg_loss: Tensor = torch.stack([x['loss'] for x in outputs]).mean()
+
+        correct: Number = sum([x['correct'] for x in outputs])
+        total: int = sum([x['total'] for x in outputs])
+
+        if isinstance(self.logger, TensorBoardLogger):
+            self.logger.experiment.add_scalar("Loss/Train",
+                                              avg_loss,
+                                              self.current_epoch)
+
+            self.logger.experiment.add_scalar("Accuracy/Train",
+                                              correct * 100/total,
+                                              self.current_epoch)
+
+    def test_step(self, batch, batch_idx: int) -> BatchDict:
+        z: Tensor = self.model(batch.x, batch.edge_index, batch.batch)
+        one_hot = torch.nn.functional.one_hot(batch.y, num_classes=2).type_as(z)
+
+        correct: Number = z.argmax(dim=1).eq(batch.y).sum().item()
+        total: int = len(batch.y)
+
+        test_loss: Tensor = self.criterion(z, one_hot)
+        test_acc: Tensor = z.argmax(dim=1).eq(batch.y).sum()/len(batch.y)
 
         self.log("test_acc", test_acc*100, prog_bar=True)
 
