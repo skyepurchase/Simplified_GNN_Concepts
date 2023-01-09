710e066affab07a10be4dfc0e894fadfc1d5e590
Skye Purchase <atp45@cam.ac.uk>
2023-01-09 16:13:30 +0000

    New mass test script to strip and store only test accuracy from multiple runs

--- scripts/base_all.sh
+++ scripts/base_all.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# Run a set of 10 runs with the prechosen seeds
+timestamp=$(date +"%m-%d-%T")
+out_dir="output/EXPR_NAME"
+out_path="$out_dir/$timestamp"   # output path based on time
 
-./run/EXPR_NAME.sh 510223
-./run/EXPR_NAME.sh 873616
-./run/EXPR_NAME.sh 557633
-./run/EXPR_NAME.sh 633554
-./run/EXPR_NAME.sh 493164
-./run/EXPR_NAME.sh 374916
-./run/EXPR_NAME.sh 80712
-./run/EXPR_NAME.sh 863742
-./run/EXPR_NAME.sh 374967
-./run/EXPR_NAME.sh 179277
+if [[ ! -d $out_dir ]]; then     # create directory for experiment
+    mkdir $out_dir
+fi
+
+touch $out_path                 # initialise empty output file
+echo "Saving runs to $out_path"
+
+# Run a set of 10 runs with the prechosen seeds
+# Using grep and sed extract just the test accuracy
+
+./run/EXPR_NAME.sh 510223 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" > $out_path &
+./run/EXPR_NAME.sh 873616 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 557633 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 633554 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 493164 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 374916 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 80712  | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 863742 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 374967 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
+./run/EXPR_NAME.sh 179277 | grep "test_acc" | sed -r "s/[ ]*test_acc[ ]*([0-9.]*)/\1/" >> $out_path &
