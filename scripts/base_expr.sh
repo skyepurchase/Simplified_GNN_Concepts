9d4c99a77d5142fd2e30db47e539bed943d9a355
Skye Purchase <atp45@cam.ac.uk>
2023-01-09 12:15:51 +0000

    Adding verbosity argument to base_expr

--- scripts/base_expr.sh
+++ scripts/base_expr.sh
@@ -7,8 +7,8 @@ for i in "${@}"; do length+=1; done             # count number of arguments
 # When running with no arguments use default
 # Seeded runs are used for batched train-test in .../scripts/run_<expr_name>
 
-if [ $length==0 ]; then
-    python3 src/main.py --config CONFIG_PATH    # placeholders for specific experiment runs
+if [ $length = 0 ]; then
+    python3 src/main.py -v --config CONFIG_PATH    # placeholders for specific experiment runs
 else
     python3 src/main.py --config CONFIG_PATH --seed $1
 fi
