5976c0e957701d6ccdf67aea7136344b5ce34c6e
Skye Purchase <atp45@cam.ac.uk>
2023-01-09 10:45:01 +0000

    Adding hooks to extract and store the output features of each layer

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,33 @@
-import torch.nn as nn
 from .gcn import GCN
 from .sgc import SGC
+from .layers import Pool
+
+# Typing
+from typing import Callable
+from torch import nn
+from torch import Tensor
+from torch_geometric.nn import GCNConv
+
+
+ACTIVATION_DICT = {}
+def _get_activation(idx: str) -> Callable:
+    def hook(model: nn.Module, in_feats: tuple, out_feats: Tensor):
+        ACTIVATION_DICT[idx] = out_feats.detach()
+    return hook
+
+
+def register_hooks(model: nn.Module) -> nn.Module:
+    for name, m in model.named_modules():
+        print(name, type(m))
+        if isinstance(m, (GCNConv, nn.Linear, Pool)):
+            print("yes")
+            m.register_forward_hook(_get_activation(f'{name}'))
+
+    return model
+
+
+def get_activation_dict() -> dict:
+    return ACTIVATION_DICT
 
 
 def get_model(name: str,
