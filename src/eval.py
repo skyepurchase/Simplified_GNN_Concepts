6c8080f97774f9ea528f78d0f0f868cb9b664aa2
Skye Purchase <atp45@cam.ac.uk>
2023-01-19 18:44:37 +0000

    Calculating and saving completeness scores

--- src/eval.py
+++ src/eval.py
@@ -1,5 +1,6 @@
 import pickle
 import os.path as osp
+from os import mkdir
 from argparse import Namespace
 from networkx import Graph
 
@@ -12,7 +13,7 @@ from datasets import get_dataset
 
 # Typing
 from sklearn.cluster import KMeans
-from torch import Tensor, ge
+from torch import Tensor
 
 from models.activation_classifier import Activation_Classifier
 
@@ -21,7 +22,8 @@ DIR = osp.dirname(__file__)
 
 
 def main(args: Namespace,
-         dataset_name: str) -> None:
+         dataset_name: str,
+         save_name: str) -> None:
     print("ONLY TESTED FOR SYNTHETIC ACTIVATIONS")
 
     dataset: InMemoryDataset = get_dataset(dataset_name,
@@ -32,7 +34,10 @@ def main(args: Namespace,
     else:
         raise ValueError(f'Expected dataset at index zero to be type {Data} received type {type(temp)}')
 
-    save_path: str = osp.join(DIR, "../output", dataset_name)
+    save_path: str = osp.join(DIR, "../output", save_name)
+
+    if not osp.exists(save_path):
+        mkdir(save_path)
 
     activation_list: dict[str, Tensor]
     with open(osp.join(DIR, "..", args.activation), 'rb') as file:
@@ -44,32 +49,33 @@ def main(args: Namespace,
     _, model_list = kmeans_cluster(activation_list, args.clusters)
 
     layer_graphs: dict[str, dict[int, list[Graph]]] = {}
-    for layer, model in model_list.items():
-        print(f"Layer {layer}")
-        layer_num = int(layer.split('.')[-1])
-        sample_graphs: dict[int, list[Graph]] = plot_samples(model,
-                                                             activation_list[layer],
-                                                             data.y,
-                                                             layer_num,
-                                                             args.clusters,
-                                                             "KMeans-Raw",
-                                                             args.num_graphs,
-                                                             data.edge_index.detach().numpy().T,
-                                                             args.hops,
-                                                             save_path)
+    with open(osp.join(save_path, f"{args.clusters}k-{args.hops}n-completeness.txt"), "w") as file:
+        for layer, model in model_list.items():
+            classifier = Activation_Classifier(model_list[layer],
+                                               activation_list[layer],
+                                               data)
+            print(f'Layer {layer} completeness: {classifier.get_accuracy()}')
+            file.write(f"{classifier.get_accuracy()}\n")
 
-        layer_graphs[layer] = sample_graphs
+            layer_num = int(layer.split('.')[-1])
+            sample_graphs: dict[int, list[Graph]] = plot_samples(model,
+                                                                 activation_list[layer],
+                                                                 data.y,
+                                                                 layer_num,
+                                                                 args.clusters,
+                                                                 "KMeans-Raw",
+                                                                 args.num_graphs,
+                                                                 data.edge_index.detach().numpy().T,
+                                                                 args.hops,
+                                                                 save_path)
 
-    if args.purity:
-        layer = input("Which layer to calculate the score on? ")
-        concept = int(input("Which concept to calculate the score on? "))
-        avg_best_score = purity(layer_graphs[layer][concept][:-1])
-        print(f'Layer {layer} Concept {concept}\navg_score: {avg_best_score}')
+            layer_graphs[layer] = sample_graphs
 
-    classifier = Activation_Classifier(model_list['layers.2'],
-                                       activation_list['layers.2'],
-                                       data)
-    print(f'Layer layers.3 completeness: {classifier.get_accuracy()}')
+        if args.purity:
+            layer = input("Which layer to calculate the score on? ")
+            concept = int(input("Which concept to calculate the score on? "))
+            avg_best_score = purity(layer_graphs[layer][concept][:-1])
+            print(f'Layer {layer} Concept {concept}\navg_score: {avg_best_score}')
 
 
 if __name__=='__main__':
@@ -86,6 +92,7 @@ if __name__=='__main__':
 
     expr_name = args.activation.split('/')[-1]
     dataset_name = expr_name.split('.')[2]
+    save_name = expr_name.split('.')[0] + "-" + dataset_name
 
-    main(args, dataset_name)
+    main(args, dataset_name, save_name)
 
