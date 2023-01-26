d772986772f94f993503beb5521127e05d53dc66
Skye Purchase <atp45@cam.ac.uk>
2023-01-26 17:28:27 +0000

    running hyperparameter search in parallel

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -4,6 +4,6 @@
 
 for config_path in $@; do
     echo $config_path
-    python3 src/optimise.py --config $config_path --root /data --min_decay 0.01 --max_decay 1.0 --min_lr 0.000001 --max_lr 0.01 --epochs 5000
+    python3 src/optimise.py --config $config_path --root /data --min_decay 0.01 --max_decay 1.0 --min_lr 0.000001 --max_lr 0.01 --epochs 5000 &
 done
 
