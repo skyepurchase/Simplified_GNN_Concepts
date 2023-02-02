d162949b27d4aec77aee8bda56266bacc5c82241
Skye Purchase <atp45@cam.ac.uk>
2023-02-02 15:57:19 +0000

    Working graph classification with SGC

--- src/wrappers/__init__.py
+++ src/wrappers/__init__.py
@@ -1,4 +1,4 @@
-from .pl_wrapper import LinearWrapper, LinearValWrapper, GraphWrapper, GraphPoolWrapper
+from .pl_wrapper import LinearPoolWrapper, LinearWrapper, LinearValWrapper, GraphWrapper, GraphPoolWrapper
 import torch.nn as nn
 
 
@@ -15,6 +15,8 @@ def get_wrapper(name: str,
         return GraphPoolWrapper(model, **config)
     if name == "GraphLinear":
         return LinearWrapper(model, **config)
+    if name == "PoolLinear":
+        return LinearPoolWrapper(model, **config)
     else:
         raise ValueError(f'Unsupported model wrapper {name}')
 
